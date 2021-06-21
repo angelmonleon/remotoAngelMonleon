73551f9a0180e0493d00c74419ae298348f6ce08
DAW <DAW@DESKTOP-ACNALSN>
2021-06-21 11:52:05 +0200

    Mensaje 2

--- Control de versiones/angelmonleon/src/angelmonleon/Clase1.java
+++ Control de versiones/angelmonleon/src/angelmonleon/Clase1.java
@@ -3,6 +3,7 @@ package angelmonleon;
 public class Clase1 {
 	public static void main(String[] args) {
 		System.out.print("Soy la linea 1 creada por Ángel Monleón");
+		System.out.print("Soy la linea 2 de la clase1 creada por Ángel Monleón");
 	}
 
 }
---
+++ Control de versiones/angelmonleon/src/angelmonleon/Clase2.java
@@ -0,0 +1,7 @@
+package angelmonleon;
+
+public class Clase2 {
+	public static void main(String[] args) {
+	System.out.print("Soy la linea 1 de la clase2 creada por Ángel Monleón");
+	}
+}
